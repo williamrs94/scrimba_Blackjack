ed8a881380f9e55eb865d63d1fcc1b3f0c6b2c9d
william <willrafael.seewald@gmail.com>
2022-05-29 20:05:39 -0300

    ready to go code

---
+++ index.js
@@ -0,0 +1,79 @@
+/* 
+A ideia do blackjack é vc ter cartas que somam a quantidade de 21 no máximo. Se passar, vc perde.
+Então temos 3 opções: 
+1) se tiver menos de 21 vc está bem
+2) se tiver exatamente 21 vc ganha
+3) se tiver mais de 21 vc perde
+
+K = 9
+Q = 10
+A = 11
+ */
+
+let player = {
+    name: "Cash",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let lossEl = document.getElementById("loss-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You lose! You'll never beat the machine!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
